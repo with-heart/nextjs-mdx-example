545ae86bf2c0a79cc8d900b31b584c0152f0d4cd
with-heart <with.heart+git@pm.me>
2022-04-25 11:38:59 -0400

    add MDX config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withMDX({
   reactStrictMode: true,
-}
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
 
 module.exports = nextConfig
